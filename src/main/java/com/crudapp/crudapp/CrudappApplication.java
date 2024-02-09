7c3f076c8de5a2c43c37c965c29a3804a71efc2f
Aditya Gupta <aditya2.gupta@PPBNOMBPBT-344.local>
2024-02-09 12:26:02 +0530

    changed commit

--- src/main/java/com/crudapp/crudapp/CrudappApplication.java
+++ src/main/java/com/crudapp/crudapp/CrudappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// todo about this annotation
 public class CrudappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/crudapp/crudapp/Csvwithoutapi.java
+++ src/main/java/com/crudapp/crudapp/Csvwithoutapi.java
@@ -12,13 +12,19 @@ import java.io.IOException;
 public class Csvwithoutapi {
     private final String csvData ="Name,Age,Location\nAditya,21,Bhagalpur\nAshutosh,22,Bhagalpur";
     private final String csvFilePath="/Users/aditya2.gupta/Downloads/project/csv/file.csv";
+
+
     @PostConstruct
     public void createCsvFile(){
         try(FileWriter writer=new FileWriter(csvFilePath)) {
             writer.write(csvData);
+            Thread.sleep(20000);
             log.info("Csv file created at {}",csvFilePath);
         } catch (IOException e) {
             log.error("Error creating Csv file",e);
-        } ;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        ;
     }
 }
